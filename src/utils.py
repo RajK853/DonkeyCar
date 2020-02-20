cfeeebbc7d8d3d890af1568d8bb8641822f34691
Raj Kumar Rana <rgk.rana@gmail.com>
2020-02-20 23:39:25 +0100

    - Progress bar added in load_data and data_gen functions - Preprocessors in data_gen vectorized to improve performance - Preprocessors parsable using parse_args

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,10 @@
 import os
 import cv2
-import json
 import argparse
 import numpy as np
+import ujson as json
 from time import time
+from numba import jit, vectorize
 from functools import wraps
 from sklearn.utils import shuffle
 from .progress_bar import ProgressBar
@@ -14,70 +15,21 @@ THROTTLE_MIN = 0.0
 THROTTLE_MAX = 5.0
 
 
+def timer_wrapper(func):
+    @wraps(func)
+    def wrapper_func(*args, **kwargs):
+        t0 = time()
+        result = func(*args, **kwargs)
+        dt = time()-t0
+        print(f"- Function '{func.__name__}' took {dt:.3f} seconds!")
+        return result
+    return wrapper_func
+
+
 def crop_img(img, crop_dim):
     assert all(isinstance(crop, slice) for crop in crop_dim), f"Crop_dim should be an slice, not {type(crop_dim)}"
     img = img[crop_dim]
     return img
-
-
-def load_img(img_file, cvt_color=None, crop_dim=None):
-    img = cv2.imread(img_file)
-    if crop_dim:
-        img = crop_img(img, crop_dim)
-    if cvt_color:
-        img = cv2.cvtColor(img, cvt_color)
-    return img
-
-
-def load_json(json_file, include_keys=None):
-    with open(json_file, "r") as json_f:
-        json_data = json.load(json_f)
-    if include_keys:
-        temp_json_data = {key: json_data[key] for key in include_keys}
-        json_data = temp_json_data
-    return json_data
-
-
-def matches_any_extension(file_name, extensions):
-    for suffix in extensions:
-        if file_name.endswith(suffix):
-            return True
-    return False
-
-
-def list_dir(dir_path, extensions=None, exclude_files=None, verbose=False):
-    files_names = os.listdir(dir_path)
-    if extensions:
-        files_names = [file_name for file_name in files_names if matches_any_extension(file_name, extensions)]
-    if exclude_files:
-        removed = 0
-        for exclude_file_name in exclude_files:
-            if exclude_file_name in files_names:
-                files_names.remove(exclude_file_name)
-                removed += 1
-            if verbose:
-                print(f"Number of files excluded: {removed}", end="\r", flush=True)
-    if verbose:
-        print(f"\nFound {len(files_names)} files with one of these extensions {extensions} in '{dir_path}'")
-    return files_names
-
-
-def load_data(data_dir, img_key, steering_key, throttle_key, extensions=None, exclude_files=None, crop_dim=None, verbose=False):
-    json_files = list_dir(data_dir, extensions=extensions, exclude_files=exclude_files, verbose=verbose)
-    json_keys = (img_key, steering_key, throttle_key)
-    img_data, actions = [], []
-    for json_file in json_files:
-        json_data = load_json(os.path.join(data_dir, json_file), include_keys=json_keys)
-        # Ignore data where the car was in rest
-        if json_data[throttle_key] == 0.0 and json_data[steering_key] == 0.0:
-            continue
-        img_path = os.path.join(data_dir, json_data[img_key])
-        # TODO: BGR2RGB conversion only required for visual purpose. Maybe use YUV format instead
-        img = load_img(img_path, cvt_color=cv2.COLOR_BGR2RGB, crop_dim=crop_dim)
-        action = np.array((json_data[steering_key], json_data[throttle_key]))
-        img_data.append(img)
-        actions.append(action)
-    return np.array(img_data), np.array(actions)
 
 
 def flip_img(img, action):
@@ -103,6 +55,67 @@ def denormalize_images(images):
     return images.astype(np.uint8)
 
 
+def load_img(img_file, cvt_color=None):
+    img = cv2.imread(img_file)
+    if cvt_color:
+        img = cv2.cvtColor(img, cvt_color)
+    return img
+
+
+def load_json(json_file, include_keys=None):
+    with open(json_file, "r") as json_f:
+        json_data = json.load(json_f)
+    if include_keys:
+        json_data = {key: json_data[key] for key in include_keys}
+    return json_data
+
+
+def matches_any_extension(file_name, extensions):
+    for suffix in extensions:
+        if file_name.endswith(suffix):
+            return True
+    return False
+
+
+def list_dir(dir_path, extensions=None, exclude_files=None, verbose=False):
+    files_names = os.listdir(dir_path)
+    if extensions:
+        files_names = [file_name for file_name in files_names if matches_any_extension(file_name, extensions)]
+    if exclude_files:
+        removed = 0
+        for exclude_file_name in exclude_files:
+            if exclude_file_name in files_names:
+                files_names.remove(exclude_file_name)
+                removed += 1
+            if verbose:
+                print(f"  Number of files excluded: {removed}", end="\r", flush=True)
+    if verbose:
+        print(f"\n  Found {len(files_names)} files with one of these extensions {extensions} in '{dir_path}'")
+    return files_names
+
+
+@timer_wrapper
+def load_data(data_dir, img_key, steering_key, throttle_key, extensions=None, exclude_files=None, verbose=False):
+    print("# Loading data")
+    json_files = list_dir(data_dir, extensions=extensions, exclude_files=exclude_files, verbose=verbose)
+    json_keys = (img_key, steering_key, throttle_key)
+    img_data, actions = [], []
+    pb = ProgressBar(total_iter=len(json_files), display_text="  Loading images", display_interval=10)
+    for json_file in json_files:
+        json_data = load_json(os.path.join(data_dir, json_file), include_keys=json_keys)
+        throttle = json_data[throttle_key] or 0.0
+        steering = json_data[steering_key] or 0.0
+        # TODO: Ignore data where the car was in rest
+        # TODO: BGR2RGB conversion only required for visual purpose. Maybe use YUV format instead
+        img_path = os.path.join(data_dir, json_data[img_key])
+        img = load_img(img_path, cvt_color=cv2.COLOR_BGR2RGB)
+        action = (steering, throttle)
+        img_data.append(img)
+        actions.append(action)
+        pb.step(1)
+    return np.array(img_data), np.array(actions)
+
+
 def data_generator(images, actions, epochs=1, batch_size=10, preprocessors=None):
     images, actions = shuffle(images, actions)
     total_samples = len(images)
@@ -110,48 +123,41 @@ def data_generator(images, actions, epochs=1, batch_size=10, preprocessors=None)
     if preprocessors:
         assert isinstance(preprocessors, (list, tuple, set)), \
             f"Preprocessors should be an iterator, not {type(preprocessors)}"
-        _preprocessors.extend(preprocessors)
-    p_bar = ProgressBar(total_iter=len(actions)*len(_preprocessors), change_line_at_reset=False)
+        for processor in preprocessors:
+            vectorized_func = np.vectorize(processor, otypes=[np.ndarray, np.ndarray], cache=True)
+            _preprocessors.append(vectorized_func)
+    p_bar = ProgressBar(total_iter=len(actions)*len(_preprocessors), change_line_at_reset=False,
+                        display_interval=batch_size)
     for epoch in range(epochs):
-        p_bar.set_display_text(f"Epoch {epoch+1:<2}")
-        for processor in _preprocessors:
-            for offset in range(0, total_samples, batch_size):
-                batch_images = images[offset:offset+batch_size]
-                batch_actions = actions[offset:offset+batch_size]
-                if processor is not None:
-                    batch_results = [processor(img, act) for img, act in zip(batch_images, batch_actions)]
-                    batch_images, batch_actions = zip(*batch_results)
-                    batch_images = np.array(batch_images)
-                    batch_actions = np.array(batch_actions)
+        p_bar.set_display_text(f"  Epoch {epoch+1:<2}")
+        for offset in range(0, total_samples, batch_size):
+            for processor in _preprocessors:
+                batch_images = np.array(images[offset:offset+batch_size])
+                batch_actions = np.array(actions[offset:offset+batch_size])
+                if processor:
+                    batch_images, batch_actions = processor(batch_images, batch_actions)
                 # TODO: Normalization may differ for YUV format
                 batch_images = normalize_images(batch_images)
                 p_bar.step(len(batch_actions))
                 yield batch_images, batch_actions
 
 
-def clip_steering(value, mode="tf"):
-    if mode == "tf":
-        import tensorflow as tf
-        return tf.clip_by_value(value, STEER_LIMIT_LEFT, STEER_LIMIT_RIGHT)
+def clip_steering_tf(value):
+    import tensorflow as tf
+    return tf.clip_by_value(value, STEER_LIMIT_LEFT, STEER_LIMIT_RIGHT)
+
+
+def clip_steering(value):
     return np.clip(value, STEER_LIMIT_LEFT, STEER_LIMIT_RIGHT)
 
 
-def clip_throttle(value, mode="tf"):
-    if mode == "tf":
-        import tensorflow as tf
-        return tf.clip_by_value(value, THROTTLE_MIN, THROTTLE_MAX)
+def clip_throttle_tf(value):
+    import tensorflow as tf
+    return tf.clip_by_value(value, THROTTLE_MIN, THROTTLE_MAX)
+
+
+def clip_throttle(value):
     return np.clip(value, THROTTLE_MIN, THROTTLE_MAX)
-
-
-def timer_wrapper(func):
-    @wraps(func)
-    def wrapper_func(*args, **kwargs):
-        t0 = time()
-        result = func(*args, **kwargs)
-        dt = time()-t0
-        print(f"Time taken spent in {func.__name__}: {dt:.3f} seconds!")
-        return result
-    return wrapper_func
 
 
 def parse_args(mode):
@@ -162,9 +168,9 @@ def parse_args(mode):
     """
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument("--version", help="DonkeyNet version", type=int,
-                            choices=(0, 1), default=1)
+                            choices=(0, 1, 2), default=1)
     if mode.lower() == "train":
-        arg_parser.add_argument("--data_dir", help="Data directory", type=str)
+        arg_parser.add_argument("--data_dirs", help="Data directories", nargs="+", required=True)
         arg_parser.add_argument("--epochs", help="Number of epochs", type=int, default=20)
         arg_parser.add_argument("--img_key", help="Key value for image array in JSON data",
                                 type=str, default="cam/image_array")
@@ -176,8 +182,11 @@ def parse_args(mode):
                                 type=str, default=None)
         arg_parser.add_argument("--retrain_model", help="Path of model to retrain",
                                 type=str, default=None)
-        arg_parser.add_argument("--verbose", help="Verbosity", dest="verbose", action="store_true")
-        arg_parser.set_defaults(verbose=False)
+        arg_parser.add_argument("--verbose", help="Verbosity", dest="verbose", action="store_true", default=False)
+        arg_parser.add_argument("--add_flip", help="Add flip pre-processing", dest="add_flip", action="store_true",
+                                default=False)
+        arg_parser.add_argument("--add_blur", help="Add blur pre-processing", dest="add_blur", action="store_true",
+                                default=False)
         _args = arg_parser.parse_args()
         if _args.save_model_path is None:
             _args.save_model_path = os.path.join("models", f"DonkeyNetV{_args.version}Model")
@@ -186,8 +195,12 @@ def parse_args(mode):
         arg_parser.add_argument("--model_path", help="Model checkpoint directory", type=str)
         arg_parser.add_argument("--sim_rate", help="Simulation rendering frequency in Hz", type=int, default=20)
         arg_parser.add_argument("--throttle", help="Car throttle value", type=float, default=0.2)
-        arg_parser.add_argument("--recording_path", help="Path to save any recording data",
-                                type=str, default=None)
+        arg_parser.add_argument("--recording_path", help="Path to save any recording data", type=str, default=None)
+        donkey_gym_envs = ["donkey-generated-roads-v0", "donkey-warehouse-v0",
+                           "donkey-avc-sparkfun-v0", "donkey-generated-track-v0"]
+        arg_parser.add_argument("--env_name", help="Donkey Gym environment name", type=str,
+                                default="donkey-generated-track-v0",
+                                choices=donkey_gym_envs)
         _args = arg_parser.parse_args()
         if _args.recording_path is not None:
             os.makedirs(_args.recording_path, exist_ok=True)

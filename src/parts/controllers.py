6d471962eed59920e7f90c611416fce74f2bd186
Raj Kumar Rana <rgk.rana@gmail.com>
2020-02-27 22:14:46 +0100

    Normalize input image array before feeding to the DonkeyNet

--- src/parts/controllers.py
+++ src/parts/controllers.py
@@ -1,24 +1,22 @@
 import os
 import tensorflow as tf
 from ..model import DonkeyNet
-from ..utils import crop_img
+from ..utils import normalize_images
 
 
 class DonkeyNetController:
     def __init__(self, sess, input_shape, model_path, version=1, throttle=0.15):
         self.sess = sess
         self.throttle = throttle
+        self.version = version
         self.input_shape = input_shape
         self.model = DonkeyNet(version=version, input_shape=input_shape)
-        # TODO: Initialize global variables somewhere
-        if not tf.__version__.startswith("2"):
-            sess.run(tf.global_variables_initializer())
+        sess.run(tf.global_variables_initializer())
         self.model.restore_model(sess, os.path.join(model_path, "model.chkpt"))
 
     def run(self, img_array):
-        # Cropping layer added in tf instead
-        # img_array = [crop_img(img_array, crop_dim=(slice(20, -1),))]
-        steering = self.model.predict(self.sess, [img_array])
+        norm_img_array = normalize_images(img_array)
+        steering = self.model.predict(self.sess, [norm_img_array])
         return steering, self.throttle
 
     def shutdown(self):

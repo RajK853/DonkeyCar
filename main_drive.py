08b3377e3822a3f2e57ed4e7392a8559e921fa4f
Raj Kumar Rana <rgk.rana@gmail.com>
2020-03-02 17:57:02 +0100

    Minor changes

--- main_drive.py
+++ main_drive.py
@@ -3,12 +3,12 @@ from donkeycar import Vehicle
 import tensorflow.compat.v1 as tf_v1
 from donkeycar.parts.camera import PiCamera
 from donkeycar.parts.datastore import TubHandler
-from donkeycar.parts.web_controller import LocalWebController
+from donkeycar.parts.controller import LocalWebController
 from donkeycar.parts.actuator import PCA9685, PWMSteering, PWMThrottle
 
 from src.pid_controller import PIDController
-from src.utils import ContextManagerWrapper, parse_args
-from src.parts import DonkeyNetController, DriveSelector
+from src.utils import ContextManagerWrapper, parse_args, get_camera
+from src.parts import DonkeyNetController, DriveSelector, ThrottleGPIOController
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
@@ -20,9 +20,11 @@ STEERING_LEFT_PWM = 100
 STEERING_RIGHT_PWM = 260
 # Throttle variables
 THROTTLE_CHANNEL_LEFT = 3
-THROTTLE_FORWARD_PWM_LEFT = 4000
-THROTTLE_STOPPED_PWM_LEFT = 2
-THROTTLE_REVERSE_PWM_LEFT = 1
+THROTTLE_FORWARD_PWM_LEFT = 1500
+THROTTLE_STOPPED_PWM_LEFT = 100
+THROTTLE_REVERSE_PWM_LEFT = 0
+# Image variables
+IMAGE_RESOLUTION = (IMAGE_H, IMAGE_W, IMAGE_D) = (120, 160, 3)
 
 if __name__ == "__main__":
     args = parse_args(mode="drive")
@@ -30,26 +32,7 @@ if __name__ == "__main__":
     TF_CONFIG = tf_v1.ConfigProto(gpu_options=GPU_OPTIONS, allow_soft_placement=True)
     with tf_v1.Session(config=TF_CONFIG) as sess:
         with ContextManagerWrapper(Vehicle(), exit_method="stop") as car:
-            if args.cam_type == "pi_cam":
-                img_resolution = (120, 160)
-                cam = PiCamera(resolution=img_resolution, framerate=args.sim_rate)
-                car.add(cam,
-                        outputs=["cam/image_array"],
-                        threaded=True)
-                # Steering and throttle controllers
-                steering_controller = PCA9685(STEERING_CHANNEL)
-                steering = PWMSteering(controller=steering_controller,
-                                       left_pulse=STEERING_LEFT_PWM,
-                                       right_pulse=STEERING_RIGHT_PWM)
-                car.add(steering, inputs=['steering'])
-
-                throttle_controller = PCA9685(THROTTLE_CHANNEL_LEFT)
-                throttle = PWMThrottle(controller=throttle_controller,
-                                       max_pulse=THROTTLE_FORWARD_PWM_LEFT,
-                                       zero_pulse=THROTTLE_STOPPED_PWM_LEFT,
-                                       min_pulse=THROTTLE_REVERSE_PWM_LEFT)
-                car.add(throttle, inputs=['throttle'])
-            elif args.cam_type == "donkey_gym":
+            if args.cam_type == "donkey_gym":
                 from donkeycar.parts.dgym import DonkeyGymEnv
                 cam = DonkeyGymEnv(SIM_PATH, port=9090, headless=0, env_name=args.env_name)
                 input_shape = cam.env.observation_space.shape
@@ -58,7 +41,12 @@ if __name__ == "__main__":
                         outputs=["cam/image_array"],
                         threaded=True)
             else:
-                raise TypeError(f"Invalid camera type: {args.camera_type}")
+                input_shape = IMAGE_RESOLUTION
+                Camera = get_camera(args.cam_type)
+                cam = Camera(image_w=IMAGE_W, image_h=IMAGE_H, image_d=IMAGE_D, framerate=args.sim_rate)
+                car.add(cam,
+                        outputs=["cam/image_array"],
+                        threaded=True)
 
             print("Web controller available at: localhost:{WEB_CONTROLLER_PORT}")
             car.add(LocalWebController(),
@@ -70,7 +58,7 @@ if __name__ == "__main__":
                                                  version=args.version, throttle=args.throttle)
             car.add(donkey_net_ctr,
                     inputs=["cam/image_array"],
-                    outputs=["random/steering", "random/throttle"],
+                    outputs=["donkeynet/steering", "donkeynet/throttle"],
                     run_condition="drive/auto")
 
             # TODO: Remove/refactor PID controller?
@@ -78,8 +66,26 @@ if __name__ == "__main__":
             print(f"PID values: {pid_values}")
             pid_ctr = PIDController(**pid_values, min_value=-1.0, max_value=1.0, store_n_errors=10, target_value=0.0)
             car.add(DriveSelector(pid_ctr),
-                    inputs=["user/steering", "user/throttle", "random/steering", "random/throttle", "user/mode"],
+                    inputs=["user/steering", "user/throttle", "donkeynet/steering", "donkeynet/throttle", "user/mode"],
                     outputs=["steering", "throttle", "drive/auto"])
+
+            if args.cam_type != "donkey_gym":
+                # Steering and throttle controllers
+                steering_controller = PCA9685(STEERING_CHANNEL)
+                steering = PWMSteering(controller=steering_controller,
+                                       left_pulse=STEERING_LEFT_PWM,
+                                       right_pulse=STEERING_RIGHT_PWM)
+                car.add(steering, inputs=["steering"])
+
+                throttle_gpio_ctr = ThrottleGPIOController(pwm=7, ain1=12, ain2=11, stby=13)
+                car.add(throttle_gpio_ctr, inputs=["throttle"], outputs=["gpio_ctr/throttle"])
+
+                throttle_controller = PCA9685(THROTTLE_CHANNEL_LEFT)
+                throttle = PWMThrottle(controller=throttle_controller,
+                                       max_pulse=THROTTLE_FORWARD_PWM_LEFT,
+                                       zero_pulse=THROTTLE_STOPPED_PWM_LEFT,
+                                       min_pulse=THROTTLE_REVERSE_PWM_LEFT)
+                car.add(throttle, inputs=["gpio_ctr/throttle"])
 
             if args.recording_path:
                 tub_handler = TubHandler(path=args.recording_path)

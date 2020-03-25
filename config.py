da22a40333ebb708e0e303ca7cde79cd4fae6094
Raj Kumar Rana <rgk.rana@gmail.com>
2020-03-25 18:23:26 +0100

    Configurations added for the updated version (i.e. classifier, sensor etc)

--- config.py
+++ config.py
@@ -1,17 +1,19 @@
 # Simulation variables
 SIM_PATH = r"/home/rajk/Machine_Learning/DonkeySimLinux/donkey_sim.x86_64"
-SIM_RATE = 60
+SIM_RATE = 20
+# Dataset variables
+DATA_TYPES = ("train", "test", "validation")
 # Steering variables
 STEERING_CHANNEL = 0
-STEERING_LEFT_PWM = 110
-STEERING_RIGHT_PWM = 260
+STEERING_LEFT_PWM = 180
+STEERING_RIGHT_PWM = 310
 # Throttle variables
 THROTTLE_CHANNEL_LEFT = 3
 THROTTLE_FORWARD_PWM_LEFT = 4000
 THROTTLE_STOPPED_PWM_LEFT = 100
 THROTTLE_REVERSE_PWM_LEFT = 0
 # Camera variables
-CAM_TYPE = "web_cam"
+CAM_TYPE = "pi_cam"                     # Camera type: web_cam|donkey_gym|pi_cam
 IMAGE_RESOLUTION = (IMAGE_H, IMAGE_W, IMAGE_D) = (120, 160, 3)
 # Joystick
 CONTROLLER_TYPE = "web_ctr"              # web_ctr|ps3
@@ -21,13 +23,17 @@ JOYSTICK_STEERING_SCALE = 1.0,
 AUTO_RECORD_ON_THROTTLE = False,
 JOYSTICK_DEADZONE = 0.0
 # Train variables
-INCLUDE_THROTTLE = True
-INCLUDE_SENSORS = False
-SENSOR_NUM = 7
 IMG_KEY = "cam/image_array"
 STEERING_KEY = "steering"
 THROTTLE_KEY = "throttle"
-SENSOR_KEYS = ["sensor_fr", "sensor_mr", "sensor_br", "sensor_bm", "sensor_bl", "sensor_ml", "sensor_fl"]
+SENSOR_KEYS = ["sensor_fr", "sensor_mr", "sensor_br", "sensor_bl", "sensor_ml", "sensor_fl"]
+# SENSOR_KEYS = ["sensor_fr", "sensor_mr", "sensor_br", "sensor_bm", "sensor_bl", "sensor_ml", "sensor_fl"]
 # Ultrasonic sensors trigger and echo pins in gpio board mode
-SENSOR_PINS = [(40, 42), (40, 42), (40, 42), (40, 42), (40, 42), (40, 42), (40, 42)]
-SEQUENCE_LENGTH = 1
+# SENSOR_PINS = [(38, 40), (32, 36), (24, 26), (18, 22), (10, 16), (37, 35), (31, 33)]
+SENSOR_PINS = [(38, 40), (32, 36), (24, 26), (10, 16), (37, 35), (31, 33)]
+SENSOR_NUM = len(SENSOR_KEYS)
+TRIG_TIME = 100e-6
+SENSOR_TIMEOUT = 0.3    # seconds
+MAX_DISTANCE = 2.0      # meters
+# Classifier
+threshold_confidence = 0.8

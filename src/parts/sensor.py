4ca933ba19679eba18a85e26dfb5968e5881efc1
Raj Kumar Rana <rgk.rana@gmail.com>
2020-03-25 22:01:10 +0100

    Implemented timer while waiting for echo by Ultrasonic sensors

--- src/parts/sensor.py
+++ src/parts/sensor.py
@@ -2,6 +2,8 @@ import RPi.GPIO as GPIO
 from time import time, sleep
 from numpy.random import uniform
 
+from . import BasePart
+
 LOW = GPIO.LOW
 HIGH = GPIO.HIGH
 OUT = GPIO.OUT
@@ -9,91 +11,82 @@ IN = GPIO.IN
 MAX_DISTANCE = 3.6  # Unit: [m]
 
 
-class UltraSonic(object):
-    def __init__(self, trig_pin, echo_pin, trig_time=1e-6, mode=GPIO.BOARD):
+class FakeSensor(BasePart):
+    def __init__(self, name, max_distance=MAX_DISTANCE):
+        self.name = name
+        self.running = True
+        self.max_distance = max_distance
+        self._distance = max_distance
+
+    @property
+    def distance(self):
+        return self._distance
+
+    @distance.setter
+    def distance(self, value):
+        dist = min(value, self.max_distance)
+        dist = round(dist, 4)
+        self._distance = dist
+
+    def calculate_distance(self):
+        self.distance = uniform(0.6, self.max_distance)
+
+    def update(self):
+        while self.running:
+            self.calculate_distance()
+
+    def run_threaded(self):
+        return self.distance
+
+    def run(self):
+        self.calculate_distance()
+        distance = self.distance
+        return distance
+
+    def shutdown(self):
+        self.running = False
+
+
+class UltraSonic(FakeSensor):
+    def __init__(self, name, trig_pin, echo_pin, max_distance, trig_time=10e-6, timeout_t=0.2, mode=GPIO.BOARD):
+        super(UltraSonic, self).__init__(name, max_distance)
         self.trig_pin = trig_pin
         self.echo_pin = echo_pin
         self.trig_time = trig_time
-        self._distance = MAX_DISTANCE
+        self.timeout_t = timeout_t
         GPIO.setmode(mode)
         self.setup()
-        self.running = True
 
     def setup(self):
         GPIO.setup(self.trig_pin, OUT)
         GPIO.setup(self.echo_pin, IN)
 
     def gen_pulse(self):
+        sleep(2*self.trig_time)
         GPIO.output(self.trig_pin, HIGH)
         sleep(self.trig_time)
         GPIO.output(self.trig_pin, LOW)
 
-    @property
-    def distance(self):
-        return self._distance
-
-    @distance.setter
-    def distance(self, value):
-        dist = min(value, MAX_DISTANCE)
-        dist = round(dist, 4)
-        self._distance = dist
-
     def calculate_distance(self):
-        self.gen_pulse()
-        echo_pin = self.echo_pin
         t0 = t1 = 0.0
-        while GPIO.input(echo_pin) == 0:
+        timeout_time = time() + self.timeout_t
+        self.gen_pulse()
+        while GPIO.input(self.echo_pin) == 0:
             t0 = time()
-        while GPIO.input(echo_pin) == 1:
+            if t0 > timeout_time:
+                print(f"Timeout in {self.name}. Returned value: {self.distance}")
+                return
+        while GPIO.input(self.echo_pin) == 1:
             t1 = time()
+            if t1 > timeout_time:
+                print(f"Timeout in {self.name}. Returned value: {self.distance}")
+                return
         dt = t1 - t0
         distance = 171.5 * dt  # Equals to (343 [m/s] * dt [s])/2
         self.distance = distance
-
-    def update(self):
-        while self.running:
-            self.calculate_distance()
-
-    def run_threaded(self):
-        return self.distance
-
-    def run(self):
-        self.calculate_distance()
-        return self.distance
 
     def shutdown(self):
         self.running = False
         GPIO.cleanup()
 
 
-class FakeSensor(object):
-    def __init__(self):
-        self.running = True
-        self._distance = MAX_DISTANCE
-
-    @property
-    def distance(self):
-        return self._distance
-
-    @distance.setter
-    def distance(self, value):
-        dist = min(value, MAX_DISTANCE)
-        dist = round(dist, 4)
-        self._distance = dist
-
-    def calculate_distance(self):
-        self.distance = uniform(0.6, MAX_DISTANCE)
-
-    def update(self):
-        while self.running:
-            self.calculate_distance()
-
-    def run_threaded(self):
-        return self.distance
-
-    def run(self):
-        self.calculate_distance()
-        return self.distance
-
-    def shutdown(self):
-        self.running = False
